2c89aa44296f524695677ca53a971395062f79ec
Kubota <suk4.atc@gmail.com>
2017-01-29 08:19:38 +0000

    コメントメッセージを入力してください

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = {host: 'elites-devise-auth-suk04.c9users.io'}
+  #応用１追加
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
